f96f87a71bc6a9433277592af5c9d8cd0b35f38f
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-18 00:56:59 +0530

    question and answer page added

---
+++ testlist.php
@@ -0,0 +1,56 @@
+<?php
+    session_start();
+    require("connection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Test list</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+<?php
+    extract($_GET);
+    //check for valid sign in
+    if(!isset($_SESSION['username'])){
+        echo "<div class='text-center'><p class='display-3'>Please Login!</p><br>";
+        echo "<u><a href='index.php'>Go Home</a></u>";
+        exit;
+    }
+    ?>
+    
+    <!-- Header -->
+    <div class="row m-2 text-center">
+        <h1 class="col-11">List of Tests</h1>
+        <div class="col-1">
+            <a href="student_corner.php"><button class="btn btn-secondary mt-2">Back</button></a> 
+        </div>
+    </div>
+    <hr class="cloud">
+
+    <!-- body -->
+    <div class='container'>
+        <h2><u>List of Tests</u></h2>
+        
+        <?php
+            $query = "select * from tests where sub_id = $sub";
+            $result = $conn->query($query);
+            if($result -> num_rows > 0){
+            $sno = 1;
+            while($row = $result->fetch_assoc()) {
+                echo $sno.". ";
+                echo "<a href=test.php?id=".$row['test_id'].">";
+                echo $row['test_name']."</a><br>";
+                $sno ++;
+            }
+        }
+            
+            
+        ?>
+    <div>
+</body>
+</html>

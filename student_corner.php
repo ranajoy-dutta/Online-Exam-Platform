39828ed98fe9b23bef8cabbd20e24601738230ff
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-17 21:26:35 +0530

    student dashboard

---
+++ student_corner.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Student Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+    <div class="row m-2 text-center ">
+        <h1 class="col-11">Student Dashboard</h1>
+        <div class="col-1">
+            <a href="logout.php"><button class="btn btn-secondary mt-2">Logout</button></a> 
+        </div>
+    </div>
+    <hr class="cloud">
+</body>
+</html>

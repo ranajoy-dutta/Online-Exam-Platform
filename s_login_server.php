8426294c71f91b98c6dfac0d0f06aa352dc3fed4
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-17 23:16:03 +0530

    Added subjects to student dashboard

--- s_login_server.php
+++ s_login_server.php
@@ -1,38 +1,44 @@
 <?php 
   require('connection.php');
-  echo "<p class ='footer'>";
+
+  function redirect($url) {
+    ob_start();
+    header('Location: '.$url);
+    ob_end_flush();
+    die();
+  }
+
   if (isset($_POST["loginid"]) && isset($_POST["password"])) {
+  //extracting password from database
   $sqlquery = "select username, password from student_login where SID = '".$_POST["loginid"]."'";
   $result = $conn->query($sqlquery);
 
+  //verifying password
   if ($result->num_rows == 1) {
     $row = $result->fetch_assoc();
     if ($row["password"] === $_POST["password"]){
+      session_start();
       $_SESSION['username'] = $row["username"];
-      echo "<b>username:</b> " . $_SESSION['username']. " <b>|| password:</b> " . $row["password"];
-      function redirect($url) {
-        ob_start();
-        header('Location: '.$url);
-        ob_end_flush();
-        die();
-      }
+      echo 'Please wait! You are being redirecting!';
+      //echo "<b>username:</b> " . $_SESSION['username']. " <b>|| password:</b> " . $row["password"];
+
+      //redirecting to student dashboard
       redirect('student_corner.php');
     }
     else {
+      //invalid password
       echo '<script language="javascript">';
       echo 'alert("Invalid Credentials!")';
       echo '</script>';
-      echo "<script> location.href='index.php'; </script>";
-      exit;
+      redirect('index.php');
   }
   } else {
+    //invalid login id
     echo '<script language="javascript">';
     echo 'alert("Invalid Login ID!")';
     echo '</script>';
-    echo "<script> location.href='index.php'; </script>";
-      exit;
+    redirect('index.php');
   }
   }
     echo "</p>";
-  ?>
-  </footer> 
+  ?>  
--- student_corner.php
+++ student_corner.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require("connection.php");
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +14,16 @@
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
 </head>
 <body>
+    <?php
+    //check for valid sign in
+    if(!isset($_SESSION['username'])){
+        echo "<div class='text-center'><p class='display-3'>Please Login!</p><br>";
+        echo "<u><a href='index.php'>Go Home</a></u>";
+        exit;
+    }
+    ?>
+
+    <!-- Header -->
     <div class="row m-2 text-center ">
         <h1 class="col-11">Student Dashboard</h1>
         <div class="col-1">
@@ -20,5 +31,29 @@
         </div>
     </div>
     <hr class="cloud">
+
+    <!-- Body -->
+    <div class="container">
+    <h3>Questions</h3>
+    <?php
+    $sqlquery = "select sub_name from subjects";
+    $result = $conn->query($sqlquery);
+    $sno = 1;
+    while($row = $result->fetch_assoc()) {
+        echo $sno.". ";
+        echo "<a href='test.php?subject=".$row['sub_name']."/'>";
+        echo $row["sub_name"]."</a><br>";
+        $sno ++;
+    }
+    ?>
+    </div>
+
+
+    <!-- Footer -->
+    <footer class="navbar fixed-bottom bg-faded ">
+    <?php
+    echo 'User : '.$_SESSION['username'];
+    ?>
+  </footer>
 </body>
 </html>

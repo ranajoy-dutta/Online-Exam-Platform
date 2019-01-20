441ed57ef1898e8217ef1c746f586df33b78389d
Ranajoy Dutta <ranajoydutta7@gmail.com>
2019-01-20 16:29:06 +0530

    DATABASE tables and data

---
+++ portal.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2019 at 11:56 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `que_id` int(11) NOT NULL,
+  `test_id` varchar(100) NOT NULL,
+  `que_desc` mediumtext NOT NULL,
+  `choice1` varchar(10000) NOT NULL,
+  `choice2` varchar(10000) NOT NULL,
+  `choice3` varchar(10000) NOT NULL,
+  `choice4` varchar(10000) NOT NULL,
+  `correct_answer` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`que_id`, `test_id`, `que_desc`, `choice1`, `choice2`, `choice3`, `choice4`, `correct_answer`) VALUES
+(1, 'program1', 'What Default Data Type ?', 'String', 'Variant', 'Integer', 'Boolean', 2),
+(2, 'program1', 'What is Default Form Border Style ?', 'Fixed Single', 'None', 'Sizeable', 'Fixed Diaglog', 3),
+(3, 'program1', 'Which is not type of Control ?', 'Text', 'Label', 'Check Box', 'Option button', 1),
+(4, 'program1', 'You application creates an instance of a form. What is the first event that will be triggered in the from?', 'Load', 'GotFocus', 'Instance', 'Initialise', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_login`
+--
+
+CREATE TABLE `student_login` (
+  `sno` int(11) NOT NULL,
+  `SID` varchar(50) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores Student Login Credentials';
+
+--
+-- Dumping data for table `student_login`
+--
+
+INSERT INTO `student_login` (`sno`, `SID`, `username`, `password`) VALUES
+(1, '1122', 'Ranajoy', 'hello12'),
+(2, '1233', 'Anant', 'hello12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `sub_id` int(11) NOT NULL,
+  `sub_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`sub_id`, `sub_name`) VALUES
+(1, 'Mathematics'),
+(2, 'English'),
+(3, 'Programming');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE `submissions` (
+  `sno` int(11) NOT NULL,
+  `session_id` varchar(10000) NOT NULL,
+  `test_id` varchar(50) NOT NULL,
+  `sub_ans` int(1) NOT NULL,
+  `correct_ans` int(1) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tests`
+--
+
+CREATE TABLE `tests` (
+  `test_id` varchar(50) NOT NULL,
+  `sub_id` int(11) NOT NULL,
+  `test_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tests`
+--
+
+INSERT INTO `tests` (`test_id`, `sub_id`, `test_name`) VALUES
+('math1', 1, 'Maths Test 1'),
+('prog33', 3, 'HTML language'),
+('program1', 3, 'programming'),
+('program2', 3, 'C Language test 2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`que_id`);
+
+--
+-- Indexes for table `student_login`
+--
+ALTER TABLE `student_login`
+  ADD PRIMARY KEY (`SID`),
+  ADD UNIQUE KEY `SID` (`sno`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`sub_id`);
+
+--
+-- Indexes for table `submissions`
+--
+ALTER TABLE `submissions`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `tests`
+--
+ALTER TABLE `tests`
+  ADD PRIMARY KEY (`test_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `que_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `student_login`
+--
+ALTER TABLE `student_login`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `submissions`
+--
+ALTER TABLE `submissions`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

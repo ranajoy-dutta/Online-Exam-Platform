bfb089e609f25c7b340d8b4cc823d4178c3c17e7
anantkaushik <anant.kaushik2@gmail.com>
2019-02-17 13:26:17 +0530

    Design added to login page

--- index.php
+++ index.php
@@ -14,54 +14,32 @@
   }
 ?>
 
+
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Welcome | Placement Preparation Platform</title>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-  <link href="bootstrap.min.css" rel="stylesheet" Type="text/css">
-  <link href="style.css" rel="stylesheet" type="text/css">
+  <link href="style1.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
 </head>
 
 <body>
-  <div class='text-center m-2 pr-5 pl-5'>
-    <h1 >Placement Preparation Portal</h1>
-    <hr class='cloud'>
-  <div class='row mt-5 pr-3'>
-    <div class='col-8'>
-      <h3 class='text-center'><u>Welcome</u></h3>
-    </div>
-    
-    <div class='col-4 div-right'><br>
-      <h3 class='text-center'><u>Student Login</u></h3>
-      
-      <?php
-        if (include('connection.php')){
-      ?>    
-       
-      <form class="form-horizontal mt-3" action='s_login_server.php' method='POST'>
-        <div class="form-group row">
-          <label for="login_id" class="col-4">Login ID :</label>
-          <div class="col-7">
-            <input type="text" name='loginid' class="form-control form-control-sm" id="login_id" placeholder="Enter Login ID" />
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="password" class="col-4">Password : </label>
-          <div class="col-7">
-            <input type="password" name='password' class="form-control form-control-sm" id="password" placeholder="********" />
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary btn-md">Submit</button>         
-      </form>
+  <h1 id="headername"> Placement Preparation Portal </h1>
+  <?php
+    if (include('connection.php')){
+  ?>    
+  <form class="box" action="s_login_server.php" method="POST">
+    <h1>login</h1>
+      <input type="text" name='loginid' id="username" placeholder="Enter Login ID">
+      <input type="password" name='password' id="pass" placeholder="Password">
+      <input type="submit" id="submit" value="login">
+    <!--<button type="submit" class="btn btn-primary btn-md">Submit</button>  -->       
+  </form>
 
-      <?php
-        };
-      ?>
-    </div>
-  </div>
-  </div>
-
-  
+  <?php
+    };
+  ?>
 </body>
 </html>

0f4e40b3f910d4a4f2a4b8deb64e64ed5f97d05e
anantkaushik <anant.kaushik2@gmail.com>
2019-04-09 05:56:47 +0530

    UI Changed

--- testlist.php
+++ testlist.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8" />
     <title>Test list</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min1.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head> 
 <body>
